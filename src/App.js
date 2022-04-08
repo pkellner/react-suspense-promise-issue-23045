2b42f6606e9cf55cb1a7af126561ea84c2ac1588
pkellner <peter@peterkellner.net>
2022-04-08 15:19:01 -0700

    updated to example that shows complexity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, Suspense, useTransition } from "react";
+import useSwr from "swr";
+import { restBase } from "./constants";
 
-function App() {
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+function CityDetail({ selectedCityId, cities, isPending }) {
+  const selectedCityIdLocal = selectedCityId
+    ? selectedCityId
+    : cities && !selectedCityId
+      ? cities[0].id
+      : undefined;
+  
+  const { data: city } = useSwr(
+    `${restBase}/api/city/${selectedCityIdLocal}`,
+    fetcher,
+    { suspense: true }
+  );
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="col-9">
+        {isPending ? (
+          <div>Loading because of isPending set</div>
+        ) : (
+          <Suspense fallback={<div>City DETAIL</div>}>
+            <div>{JSON.stringify(city ?? "{id: 99999}")}</div>
+          </Suspense>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+function CityList() {
+  const { data: cities } = useSwr(`${restBase}/api/city`, fetcher, {
+    suspense: true,
+  });
+  
+  const [selectedCityId, setSelectedCityId] = useState();
+  const [isPending, startTransition] = useTransition();
+  
+  return (
+    <Suspense fallback={<div>Loading CityShowData...</div>}>
+      <div className="row">
+        <div className="col-3">
+          {cities.map((city) => {
+            return (
+              <div key={city.id}>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    console.log("onClick");
+                    startTransition(() => {
+                      console.log("startTransition");
+                      setSelectedCityId(city.id);
+                    });
+                  }}
+                >
+                  {city.city}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <div className="col-9">
+          <div>
+            <CityDetail
+              selectedCityId={selectedCityId}
+              cities={cities}
+              isPending={isPending}
+            />
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  );
+}
+
+function CityHeader() {
+  return (
+    <div className="row">
+      <div className="col-3">
+        <b>CITY LIST</b>
+        <hr />
+      </div>
+      <div className="col-9">
+        <div>
+          <b>CITY DETAIL</b>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="container">
+      <a href="/">Site Root</a>
+      <hr />
+      <CityHeader />
+      <CityList />
+    </div>
+  );
+}
